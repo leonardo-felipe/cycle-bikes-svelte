86b4fb64b7c384fc41f557fe99b2e41e402d6079
Leonardo Oliveira <60754433+leonardo-felipe@users.noreply.github.com>
2023-08-20 15:05:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cycle-bikes-svelte
+ 
---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,98 @@
+<script>
+
+</script>
+
+<html lang="pt-br">
+<head>
+  <title>Cycle Bikes</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<header>
+  <div class="jumbotron text-center" style="background-color: #F5F5F5;">
+    <h1 id="home" style="color: #424242">Cycles Bikes</h1>
+    <p>Bicicletas sob encomenda</p>
+    <p class="text-center"></p>
+  </div>
+</header>
+<nav>
+  <ul class="nav justify-content-center">
+    <li>
+      <div class="dropdown">
+        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" style="background-color: #424242;">&#9776;</button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#home" style="color: #424242;">Home</a>
+          <a class="dropdown-item" href="https://medium.com/@cyclesbikes" style="color: #424242;">Blog</a>
+        </div>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#sobre" style="color: #424242;">Sobre</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#produtos" style="color: #424242;">Produtos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#contato" style="color: #424242;">Contato</a>
+    </li>
+  </ul>
+</nav>
+<body>
+  <div>
+    <h3 id="sobre" style="color: #424242">Sobre</h3>
+    <p style="color: #424242">A Cycles Bikes é uma empresa que tem a missão de produzir bicicletas de alta qualidade.</p>
+  </div>
+  <div class="container mt-3">
+    <h3 id="produtos" style="color: #424242">Produtos</h3>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ul class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+   
+      </ul>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="https://cdn.pixabay.com/photo/2016/11/18/12/49/bicycle-1834265_960_720.jpg" alt="bicicleta1" width="90%" height="30%">
+        </div>
+        <div class="carousel-item">
+          <img src="https://cdn.pixabay.com/photo/2014/07/31/23/50/bicycle-407215_960_720.jpg" alt="bicicleta2" width="90%" height="30%">
+        </div>
+ 
+      </div>
+      <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+  </div>
+</body>
+<footer id="contato">
+  <h2>Contato</h2>
+  <p>Telefone: (XX) 99999-9999 <br> Email: contato@cyclesbikes.com</p>
+  <img src="https://cdn.pixabay.com/photo/2015/01/13/13/20/guy-598180__340.jpg" alt="homem-com-bicicleta">
+</footer>
+</html>
+
+<style>
+    * {
+  text-align: center;
+}
+.carousel-inner img {
+  margin: auto;
+}
+footer {
+  color: #424242;
+  background-color: #f5f5f5;
+}
+footer img {
+  border-radius: 50%;
+  width: 15%;
+}
+</style>
